7ebd657f56b2aeed0d845df20317489697ccfdd6
RitikRajoria <ritikrajrrr@gmail.com>
2022-02-04 00:35:09 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 
+class CatalogModel{
+  static final items = [
+  Item(
+    id: 1,
+    name: "asdasdas",
+    desc: "hhjdfhdjdash hsdjfhsdjh",
+    price: 898,
+    color: "#33505a",
+    image: "https://lh5.googleusercontent.com/proxy/sdVrrPW89fxuHTufgegoz1tFzqZjprNFPPecLa6ZJyCzbDHWKlSCCzN_AMVsklkMHQFgr1gpPUXGEb4GNTBTcunUTCjsO_ImJqGPRMwnjolGkEQC8f6lGS6HQfU3tscghilSBP0okPN-updG044T2dITVo-u6oUSiJFU7pKSKOfr1qO9B-yUCmPaFYvhBYKd1dYRpfhc_fBFatA=w1200-h630-p-k-no-nu",
+  ),
+];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -11,14 +24,3 @@ class Item {
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.image, required this.color}); 
 }
-
-final products = [
-  Item(
-    id: "asdasd",
-    name: "asdasdas",
-    desc: "hhjdfhdjdash hsdjfhsdjh",
-    price: 898,
-    color: "#33505a",
-    image: "https://lh5.googleusercontent.com/proxy/sdVrrPW89fxuHTufgegoz1tFzqZjprNFPPecLa6ZJyCzbDHWKlSCCzN_AMVsklkMHQFgr1gpPUXGEb4GNTBTcunUTCjsO_ImJqGPRMwnjolGkEQC8f6lGS6HQfU3tscghilSBP0okPN-updG044T2dITVo-u6oUSiJFU7pKSKOfr1qO9B-yUCmPaFYvhBYKd1dYRpfhc_fBFatA=w1200-h630-p-k-no-nu",
-  ),
-];
